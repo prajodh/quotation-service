65bd02f5d5ada57d4b42e9a5d5d9feb79af1bc71
prajodh <prajodhpragaths@gmail.com>
2024-03-07 16:30:40 -0500

    first commit

---
+++ src/db-access.js
@@ -0,0 +1,20 @@
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
+const mongodb = require('mongodb');
+let collection = undefined;
+async function connectToDB() { 
+const client = await mongodb.MongoClient.connect (DBHOST);
+const db = client.db(DBNAME); 
+quotations_collection = await db.collection("quotations");
+return quotations_collection; }
+
+async function selectAQuotation() {
+if(!collection) {
+collection = await connectToDB();
+}
+console.log(collection);
+const result = await collection.findOne({});
+return result;
+}
+
+module. exports = { selectAQuotation } 
